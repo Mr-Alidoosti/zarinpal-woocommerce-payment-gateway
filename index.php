611690ef4e57cd12fe1e008c3ea34ceda33f00a3
MohammadReza Alidoosti <mohammadreza.alidoosti77@gmail.com>
2022-02-10 12:55:42 +0330

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: افزونه پرداخت امن زرین‌پال برای ووکامرس
+Version: 4.9.0
+Description:  افزونه درگاه پرداخت امن زرین‌پال برای فروشگاه ساز ووکامرس
+Plugin URI: http://zarinpal.com
+Author: Mr.Alidoosti
+Author URI: https://mr-alidoosti.com/
+
+*/
+include_once("class-wc-gateway-zarinpal.php");
